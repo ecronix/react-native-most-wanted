361e6ee50ea987d6e8d79d397c6e35ea205eeff5
Tarik <tarik.huber@acid21.com>
2024-09-05 10:25:34 +0200

    Implemented basic splash screen

--- template/App.tsx
+++ template/App.tsx
@@ -2,10 +2,12 @@ import * as React from 'react';
 import {AppNavigation} from './src/navigation';
 import Providers from './src/navigation/providers';
 //import SplashScreen from 'react-native-splash-screen';
+import BootSplash from 'react-native-bootsplash';
 
 const App = () => {
   React.useEffect(() => {
     //SplashScreen.hide();
+    BootSplash.hide({fade: true});
   }, []);
 
   return (
